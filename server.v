dd7a2df391f4b0363e839350a0e4b8d8fa7cd94d
Hunam <hunam@disroot.org>
2022-05-28 12:51:31 +0200

    move `ctx.send()` to `server.v`
    Signed-off-by: Hunam <hunam@disroot.org>

--- server.v
+++ server.v
@@ -4,6 +4,7 @@ import net
 import net.http
 import time
 import io
+import os
 
 [heap]
 pub struct App {
@@ -35,6 +36,11 @@ pub struct Config {
 [params]
 pub struct RedirectConfig {
 	status http.Status = .found
+}
+
+[params]
+pub struct SendOpt {
+	index string // file served at `/`
 }
 
 type HandlerFn = fn (mut ctx Ctx)
@@ -102,4 +108,20 @@ pub fn (mut app App) use(middleware ...HandlerFn) {
 pub fn (mut ctx Ctx) redirect(path string, cfg RedirectConfig) {
 	ctx.res.set_status(cfg.status)
 	ctx.res.header.add(.location, path)
+}
+
+// serve static file(s)
+pub fn (mut ctx Ctx) send(dir_path string, mount_path string, opt SendOpt) {
+	if ctx.req.url.starts_with(mount_path) {
+		req_path := ctx.req.url[mount_path.len..].trim_right('/')
+		real_path := os.resource_abs_path(dir_path + req_path)
+		if os.is_file(real_path) {
+			ctx.res = file_res(real_path)
+		} else if req_path.len == 0 && opt.index.len > 0 {
+			index_real_path := os.resource_abs_path('$dir_path/$opt.index')
+			if os.is_file(index_real_path) {
+				ctx.res = file_res(index_real_path)
+			}
+		}
+	}
 }
